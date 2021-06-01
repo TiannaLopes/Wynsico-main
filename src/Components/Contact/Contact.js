c263cfa916330d4e3f4cb8214e61bbe8d1bb8203
Tianna Lopes <tiannalopes@Tiannas-iMac.fios-router.home>
2021-06-01 10:11:57 -0400

    new

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Jumbotron, Container} from 'reactstrap';
+import NavBar from '../Navbar/Navbar';
+
+function Contact() {
+  return (
+    <div className="App">
+     <div>
+      <Jumbotron fluid>
+         <NavBar />
+        <Container fluid>
+          <h1 className="display-3">My Contact Details</h1>
+          <p className="lead">Here are my contact details </p>
+          <p className="lead">Email : sachin@wynisco.com my email </p>
+          <p className="lead">Phone : 732 874 0188 </p>
+          <p className="lead">Typical Wake up / Sleep Hours : Other than bootcamp. I will available morning 7AM to 11AM EST via slack or whatsapp Changed  </p>
+        </Container>
+      </Jumbotron>
+    </div>
+    </div>
+  );
+}
+
+export default Contact;
