8f936f7f0c18aa1553b5acc7bc9c7acf07b0d632
TiannaLopes <tiannalopes@shaw.ca>
2021-06-03 16:17:47 -0400

    numbers updated

--- src/Components/Homeworks/index.js
+++ src/Components/Homeworks/index.js
@@ -67,21 +67,21 @@ function Homeworks() {
 
       </tr>
       <tr>
-          <th scope="row">5</th>
+          <th scope="row">6</th>
           <td>Homework 3.3</td>
           <td>Javascript Function</td>
           <td><a href="https://codepen.io/tiannalopes/pen/yLMoQzN"> View Result</a> </td>
           <td><a href="https://codepen.io/tiannalopes/pen/yLMoQzN"> Link to Code</a> </td>
       </tr>
       <tr>
-          <th scope="row">6</th>
+          <th scope="row">7</th>
           <td>Homework 4.1</td>
           <td>Interview Assignment Practice </td>
           <td><a href="https://codepen.io/tiannalopes/pen/ExWbYVd?editors=1111"> View Result</a> </td>
           <td><a href="https://codepen.io/tiannalopes/pen/ExWbYVd?editors=1111"> Link to Code</a> </td>
       </tr>
       <tr>
-          <th scope="row">7</th>
+          <th scope="row">8</th>
           <td>Homework 5.1</td>
           <td>Theme Change </td>
           <td><a href="https://codepen.io/tiannalopes/pen/bGqYPPz?editors=0011"> View Result</a> </td>
@@ -89,28 +89,28 @@ function Homeworks() {
       </tr>
 
       <tr>
-          <th scope="row">8</th>
+          <th scope="row">9</th>
           <td>Homework 5.2</td>
           <td>frontier Challenge </td>
           <td><a href=" https://codepen.io/tiannalopes/pen/yLMvBYg"> View Result</a> </td>
           <td><a href="https://github.com/TiannaLopes/frontierChallenge">Link to Code</a> </td>
       </tr>
       <tr>
-          <th scope="row">9</th>
+          <th scope="row">10</th>
           <td>Homework 5.3</td>
           <td>Algo Expert </td>
           <td><a href="https://codepen.io/tiannalopes/pen/bGqYPPz?editors=0011"> View Result</a> </td>
           <td><a href="https://codepen.io/tiannalopes/pen/bGqYPPz?editors=0011">Link to Code</a> </td>
 </tr>
     <tr>
-          <th scope="row">10</th>
+          <th scope="row">11</th>
           <td>Homework 6.1</td>
           <td>Cascade to vanilla javascript boilerplate</td>
           <td><a href="https://cnn.com"> View Result</a> </td>
           <td><a href="https://github.com/TiannaLopes/CascadeProject">Link to Code</a> </td>
 </tr>
     <tr>
-          <th scope="row">11</th>
+          <th scope="row">12</th>
           <td>Homework 7.1</td>
           <td>Watch Videos and Practice JS </td>
           <td><a href="https://codepen.io/tiannalopes/pen/LYWQOYo"> View Result</a> </td>
@@ -118,28 +118,28 @@ function Homeworks() {
 </tr>
 
     <tr>
-          <th scope="row">12</th>
+          <th scope="row">13</th>
           <td>Homework 8.1</td>
           <td> Teleport Exercise </td>
           <td><a href="https://github.com/TiannaLopes/teleport-acme"> View Result</a> </td>
           <td><a href="https://github.com/TiannaLopes/teleport-acme">Link to Code</a> </td>
 </tr>
  <tr>
-          <th scope="row">13</th>
+          <th scope="row">14</th>
           <td>Homework 8.2</td>
           <td> SSCSS - change theme when break point is at mobile </td>
           <td><a href="hhttps://codepen.io/tiannalopes/pen/WNpMaYo"> View Result</a> </td>
           <td><a href="https://codepen.io/tiannalopes/pen/WNpMaYo">Link to Code</a> </td>
 </tr>
  <tr>
-          <th scope="row">14</th>
+          <th scope="row">15</th>
           <td>Homework 8.3</td>
           <td> Palindrome Problem </td>
           <td><a href="https://codepen.io/tiannalopes/pen/MWpVwEJ"> View Result</a> </td>
           <td><a href="https://codepen.io/tiannalopes/pen/MWpVwEJ">Link to Code</a> </td>
 </tr>
     <tr>
-          <th scope="row">15</th>
+          <th scope="row">16</th>
           <td>Homework 10.1</td>
           <td> HTML Question</td>
           <td><a href="https://codepen.io/tiannalopes/pen/MWpQOPL"> View Result</a> </td>
